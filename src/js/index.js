b0e5fc85d194549e2633fd657c216d021a63f020
Adan <alu0100909012@ull.edu.es>
2020-05-27 18:26:07 +0100

    responsive acabado

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+console.log("works1!")
+var M;
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
